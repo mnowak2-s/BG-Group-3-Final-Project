60c97aa728a8b4344b6c37d82a67527aae4388cb
mnowak2-s <mnowak2@uw.edu>
2021-05-31 12:37:34 -0700

    Get data type widget to work

--- BG-3-Final-Project/server.R
+++ BG-3-Final-Project/server.R
@@ -11,30 +11,41 @@ shinyServer(function(input, output) {
     })
     
     ## Data for the histogram
-    edit_or_data <- Sym_of_or_df %>% 
-        filter(Group == "By State") %>% 
-        group_by(State) %>% 
-        arrange(State, Phase) %>% 
-        mutate(Diff_percent = Value - lag(Value), 
-               Diff_time = Time.Period - lag(Time.Period),
-               Rate_percent = (Diff_time / Diff_percent)/lag(Time.Period)*100,
-               Diff_percent = case_when(row_number() == 1 ~ Value, TRUE ~ Diff_percent))
-    edit_or_data$Phase <- as.factor(edit_or_data$Phase)
+    edit_or_data <- reactive({
+        if(is.null(input$typeData)){
+            Sym_of_or_df %>% 
+            filter(Group == "By State") %>% 
+            group_by(State) %>% 
+            arrange(State, Phase) %>% 
+            mutate(Diff_percent = Value - lag(Value), 
+                Diff_time = Time.Period - lag(Time.Period),
+                Rate_percent = (Diff_time / Diff_percent)/lag(Time.Period)*100,
+                Diff_percent = case_when(row_number() == 1 ~ Value, TRUE ~ Diff_percent))
+        } else {
+            dataframe%>% 
+            filter(Group == "By State", Indicator == input$typeData) %>% 
+            group_by(State) %>% 
+            arrange(State, Phase) %>% 
+            mutate(Diff_percent = Value - lag(Value), 
+                Diff_time = Time.Period - lag(Time.Period),
+                Rate_percent = (Diff_time / Diff_percent)/lag(Time.Period)*100,
+                Diff_percent = case_when(row_number() == 1 ~ Value, TRUE ~ Diff_percent))
+        }
+    })
     
     output$plot <- renderPlot({
-        ggplot(data = edit_or_data, aes(x = State, y = Diff_percent), stat=identity) +
-            geom_bar(stat = "identity", fill = edit_or_data$Phase, show.legend = TRUE) + 
+        ggplot(data = edit_or_data(), aes(x = State, y = Diff_percent)) +
+            geom_bar(stat = "identity", fill = edit_or_data()$Phase, show.legend = TRUE) + 
             geom_abline(mapping = NULL, data = NULL, slope = 0, intercept = 0, col = "white")
     })
     
     output$xValDisplay <- renderUI({
         radioButtons("xValTypes", label = "Categories of data", 
-                     choices = unique(Sym_of_or_df$Group), selected = "By State")
+                     choices = unique(edit_or_data()$Group), selected = "By State")
     })
     
     output$selectType <- renderUI({
         selectInput("typeData", label = "Type of data", 
-                    choices = list("Symptoms of Anxiety" = 1, "Symptoms of Depression" = 2, "Symptoms of Anxiety or Depression" = 3), 
-                    selected = 1)
+                    choices = unique(dataframe$Indicator))
     })
 })
