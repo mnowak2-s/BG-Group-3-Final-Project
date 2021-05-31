2833d88859a8bd34af28dabb14d7374b8ff4ffb2
mnowak2-s <mnowak2@uw.edu>
2021-05-30 18:59:43 -0700

    Add datasets and functions to site file, progress on histogram

---
+++ BG-3-Final-Project/Dataframes.R
@@ -0,0 +1,20 @@
+library(dplyr)
+
+dataframe <- read.csv('Anxiety_Depression_rates_Covid_19.csv')
+
+nrow(dataframe) # number of rows 
+ncol(dataframe)
+names(dataframe) # variable names
+
+unique(dataframe[c("Indicator")])
+
+Sym_of_anx_df <- dataframe %>% 
+  filter(Indicator == "Symptoms of Anxiety Disorder")
+
+
+Sym_of_dep_df <- dataframe %>% 
+  filter(Indicator == "Symptoms of Depressive Disorder")
+
+
+Sym_of_or_df <- dataframe %>% 
+  filter(Indicator == "Symptoms of Anxiety Disorder or Depressive Disorder")
--- BG-3-Final-Project/server.R
+++ BG-3-Final-Project/server.R
@@ -11,10 +11,16 @@ shinyServer(function(input, output) {
     })
     
     edit_or_data <- Sym_of_or_df %>% 
-        filter(Group == "By State")
+        filter(Group == "By State") %>% 
+        group_by(State, Phase) %>% 
+        arrange(State, Phase) %>% 
+        mutate(Diff_percent = Value - lag(Value), 
+               Diff_time = Time.Period - lag(Time.Period),
+               Rate_percent = (Diff_time / Diff_percent)/lag(Time.Period)*100,
+               Diff_percent = case_when(row_number() == 1 ~ Value, TRUE ~ Diff_percent))
     edit_or_data$Phase <- as.factor(edit_or_data$Phase)
     output$plot <- renderPlot({
-        ggplot(data = edit_or_data, aes(x = State, y = Value), stat=identity) +
+        ggplot(data = edit_or_data, aes(x = State, y = Diff_percent), stat=identity) +
             geom_bar(stat = "identity", fill = edit_or_data$Phase)
     })
 })
