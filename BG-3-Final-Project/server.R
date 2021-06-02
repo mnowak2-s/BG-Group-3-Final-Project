7ca0f10c00b1f0a948f15b87420352204f30041d
mnowak2-s <mnowak2@uw.edu>
2021-06-02 13:57:58 -0700

    Change graphs to represent correct things

--- BG-3-Final-Project/server.R
+++ BG-3-Final-Project/server.R
@@ -6,21 +6,49 @@ source("Dataframes.R")
 shinyServer(function(input, output) {
     
     ## Data for the histogram, need to figure out how to put widget data into this
-    edit_or_data <- reactive({
-        dataframe %>% 
-            filter(Group == "By State", Indicator == input$typeData) %>% 
-            group_by(State) %>% 
-            arrange(State, Phase) %>% 
-            mutate(Diff_percent = Value - lag(Value), 
-                   Diff_time = Time.Period - lag(Time.Period),
-                   Rate_percent = (Diff_time / Diff_percent)/lag(Time.Period)*100,
-                   Diff_percent = case_when(row_number() == 1 ~ Value, TRUE ~ Diff_percent))
+    Sym_of_or_df <- dataframe %>% 
+        filter(Group == "By State", Indicator == "Symptoms of Anxiety Disorder or Depressive Disorder") %>% 
+        group_by(State) %>% 
+        arrange(State, Phase) %>% 
+        mutate(Diff_percent = Value - lag(Value), 
+               Diff_time = Time.Period - lag(Time.Period),
+               Rate_percent = (Diff_time / Diff_percent)/lag(Time.Period)*100,
+               Diff_percent = case_when(row_number() == 1 ~ Value, TRUE ~ Diff_percent))
+    
+    Sym_of_anx_df <- dataframe %>% 
+        filter(Group == "By State", Indicator == "Symptoms of Anxiety Disorder") %>% 
+        group_by(State) %>% 
+        arrange(State, Phase) %>% 
+        mutate(Diff_percent = Value - lag(Value), 
+               Diff_time = Time.Period - lag(Time.Period),
+               Rate_percent = (Diff_time / Diff_percent)/lag(Time.Period)*100,
+               Diff_percent = case_when(row_number() == 1 ~ Value, TRUE ~ Diff_percent))
+    
+    Sym_of_dep_df <- dataframe %>% 
+        filter(Group == "By State", Indicator == "Symptoms of Depressive Disorder") %>% 
+        group_by(State) %>% 
+        arrange(State, Phase) %>% 
+        mutate(Diff_percent = Value - lag(Value), 
+               Diff_time = Time.Period - lag(Time.Period),
+               Rate_percent = (Diff_time / Diff_percent)/lag(Time.Period)*100,
+               Diff_percent = case_when(row_number() == 1 ~ Value, TRUE ~ Diff_percent))
+    
+    # Plot outputs
+    output$plot_or <- renderPlot({
+        ggplot(data = Sym_of_or_df, aes(x = State, y = Diff_percent)) + 
+            geom_bar(stat = "identity", fill = Sym_of_or_df$Phase, show.legend = TRUE) +
+            geom_abline(mapping = NULL, data = NULL, slope = 0, intercept = 0, col = "white")
     })
     
-    # Plot output
-    output$plot_anx <- output$plot_dep <- output$plot_or <- renderPlot({
-        ggplot(data = edit_or_data(), aes(x = State, y = Diff_percent)) +
-            geom_bar(stat = "identity", fill = edit_or_data()$Phase, show.legend = TRUE) + 
+    output$plot_anx <- renderPlot({
+        ggplot(data = Sym_of_anx_df, aes(x = State, y = Diff_percent)) + 
+            geom_bar(stat = "identity", fill = Sym_of_or_df$Phase, show.legend = TRUE) +
+            geom_abline(mapping = NULL, data = NULL, slope = 0, intercept = 0, col = "white")
+    })
+    
+    output$plot_dep <- renderPlot({
+        ggplot(data = Sym_of_dep_df, aes(x = State, y = Diff_percent)) + 
+            geom_bar(stat = "identity", fill = Sym_of_or_df$Phase, show.legend = TRUE) +
             geom_abline(mapping = NULL, data = NULL, slope = 0, intercept = 0, col = "white")
     })
     
