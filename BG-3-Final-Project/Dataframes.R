6018e1abf3ecb4020221afe7f0bf848c5c7b3988
mnowak2-s <mnowak2@uw.edu>
2021-06-05 20:00:54 -0700

    Edit data frame data

--- BG-3-Final-Project/Dataframes.R
+++ BG-3-Final-Project/Dataframes.R
@@ -9,12 +9,31 @@ names(dataframe) # variable names
 unique(dataframe[c("Indicator")])
 
 Sym_of_anx_df <- dataframe %>% 
-  filter(Indicator == "Symptoms of Anxiety Disorder")
+  filter(Group == "By State", Indicator == "Symptoms of Anxiety Disorder", Phase >= 0) %>% 
+  group_by(State) %>% 
+  arrange(State, Phase) %>% 
+  mutate(Diff_percent = Value - lag(Value), 
+         Diff_time = Time.Period - lag(Time.Period),
+         Rate_percent = (Diff_time / Diff_percent)/lag(Time.Period)*100,
+         Diff_percent = case_when(row_number() == 1 ~ Value, TRUE ~ Diff_percent))
+  
 
 
 Sym_of_dep_df <- dataframe %>% 
-  filter(Indicator == "Symptoms of Depressive Disorder")
+  filter(Group == "By State", Indicator == "Symptoms of Depressive Disorder", Phase >= 0) %>% 
+  group_by(State) %>% 
+  arrange(State, Phase) %>% 
+  mutate(Diff_percent = Value - lag(Value), 
+         Diff_time = Time.Period - lag(Time.Period),
+         Rate_percent = (Diff_time / Diff_percent)/lag(Time.Period)*100,
+         Diff_percent = case_when(row_number() == 1 ~ Value, TRUE ~ Diff_percent))
 
 
 Sym_of_or_df <- dataframe %>% 
-  filter(Indicator == "Symptoms of Anxiety Disorder or Depressive Disorder")
+  filter(Group == "By State", Indicator == "Symptoms of Anxiety Disorder or Depressive Disorder", Phase >= 0) %>% 
+  group_by(State) %>% 
+  arrange(State, Phase) %>% 
+  mutate(Diff_percent = Value - lag(Value), 
+         Diff_time = Time.Period - lag(Time.Period),
+         Rate_percent = (Diff_time / Diff_percent)/lag(Time.Period)*100,
+         Diff_percent = case_when(row_number() == 1 ~ Value, TRUE ~ Diff_percent))
--- BG-3-Final-Project/server.R
+++ BG-3-Final-Project/server.R
@@ -4,44 +4,16 @@ library(tidyverse)
 source("Dataframes.R")
 
 shinyServer(function(input, output) {
-    
-    Sym_of_or_df <- dataframe %>% 
-        filter(Group == "By State", Indicator == "Symptoms of Anxiety Disorder or Depressive Disorder", Phase >= 0) %>% 
-        group_by(State) %>% 
-        arrange(State, Phase) %>% 
-        mutate(Diff_percent = Value - lag(Value), 
-               Diff_time = Time.Period - lag(Time.Period),
-               Rate_percent = (Diff_time / Diff_percent)/lag(Time.Period)*100,
-               Diff_percent = case_when(row_number() == 1 ~ Value, TRUE ~ Diff_percent))
-  
-    Sym_of_anx_df <- dataframe %>% 
-        filter(Group == "By State", Indicator == "Symptoms of Anxiety Disorder", Phase >= 0) %>% 
-        group_by(State) %>% 
-        arrange(State, Phase) %>% 
-        mutate(Diff_percent = Value - lag(Value), 
-               Diff_time = Time.Period - lag(Time.Period),
-               Rate_percent = (Diff_time / Diff_percent)/lag(Time.Period)*100,
-               Diff_percent = case_when(row_number() == 1 ~ Value, TRUE ~ Diff_percent))
-    
-    Sym_of_dep_df <- dataframe %>% 
-        filter(Group == "By State", Indicator == "Symptoms of Depressive Disorder", Phase >= 0) %>% 
-        group_by(State) %>% 
-        arrange(State, Phase) %>% 
-        mutate(Diff_percent = Value - lag(Value), 
-               Diff_time = Time.Period - lag(Time.Period),
-               Rate_percent = (Diff_time / Diff_percent)/lag(Time.Period)*100,
-               Diff_percent = case_when(row_number() == 1 ~ Value, TRUE ~ Diff_percent))
-    
     # Plot outputs
-    Sym_of_or_df <- Sym_of_or_df%>% 
-      group_by(State, Phase)%>% 
+    Sym_of_or_df <- Sym_of_or_df %>% 
+      group_by(State, Phase) %>% 
       mutate(avg = mean(Value))
     
     output$plot_or <- renderPlot({
-        small_or_df <- Sym_of_or_df%>% 
-          select(State, avg)%>%
-          filter(Phase == input$orPhase)%>%
-          group_by(State)%>%
+        small_or_df <- Sym_of_or_df %>% 
+          select(State, avg) %>%
+          filter(Phase == input$orPhase) %>%
+          group_by(State) %>%
           distinct()
         ggplot(data = small_or_df) + 
             geom_bar(mapping = aes(x = reorder(State, avg), y = avg), stat = "identity",  show.legend = TRUE)+
